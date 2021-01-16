13eb767c8de18f323ea0e4c2653333e5bf257fe9
zeddan <robin.saaf@gmail.com>
2021-01-16 11:47:12 +0100

    Make into Firefox addon
    First working version

---
+++ hardwax-autoplay.js
@@ -0,0 +1,53 @@
+"use strict";
+
+function playRecord(record) {
+  if (record) {
+    record.querySelectorAll("ul.tracklisting")[0].children[0].getElementsByTagName("a")[0].click();
+  };
+}
+
+function isPlaying(record) {
+  return (record.querySelectorAll("ul.tracklisting")[0].querySelectorAll("li.playing").length > 0);
+}
+
+function stop() {
+  let currentTrack = records[currentRecordIdx].querySelectorAll("ul.tracklisting")[0].querySelectorAll("li.playing")
+
+  if (currentTrack.length > 0) {
+    currentTrack[0].getElementsByTagName("a")[0].click();
+  }
+
+  clearInterval(runningId);
+}
+
+let runningId = -1;
+let currentRecordIdx = -1;
+let records = []
+
+function run() {
+  currentRecordIdx = 0
+  records = document.querySelectorAll('*[id^="record"]');
+  let retries = 0;
+
+  playRecord(records[currentRecordIdx]);
+
+  runningId = setInterval(() => {
+    if (currentRecordIdx < records.length || retries > 5) {
+      if (!isPlaying(records[currentRecordIdx])) {
+        currentRecordIdx += 1;
+        retries += 1;
+        playRecord(records[currentRecordIdx]);
+      };
+    } else {
+      clearInterval(i);
+    };
+  }, 5000);
+}
+
+browser.runtime.onMessage.addListener(menu => {
+  if      (menu.command === "run")   { run();	  }
+  else if (menu.command === "stop")  { stop();  }
+  else if (menu.command === "pause") { pause(); }
+  else if (menu.command === "reset") { reset(); }
+});
+
---
+++ menu.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function onError(error) {
+  alert(error);
+  console.error(`Error: ${error}`);
+}
+
+function sendCommand(tabs, command) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {command: command}
+    ).catch(onError);
+  }
+}
+
+document.getElementById("run").addEventListener("click", (e) => {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then((tabs) => sendCommand(tabs, "run")).catch(onError);
+});
+
+document.getElementById("stop").addEventListener("click", (e) => {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then((tabs) => sendCommand(tabs, "stop")).catch(onError);
+});
+
+document.getElementById("pause").addEventListener("click", (e) => {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then((tabs) => sendCommand(tabs, "pause")).catch(onError);
+});
+
+document.getElementById("reset").addEventListener("click", (e) => {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then((tabs) => sendCommand(tabs, "reset")).catch(onError);
+});
+
