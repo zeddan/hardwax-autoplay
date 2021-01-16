1db4c8c03d4e28c30bc76a1e4528997490dda615
zeddan <robin.saaf@gmail.com>
2021-01-16 13:05:55 +0100

    Make function for registering commands

--- menu.js
+++ menu.js
@@ -14,31 +14,17 @@ function sendCommand(tabs, command) {
   }
 }
 
-document.getElementById("run").addEventListener("click", (e) => {
-  browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then((tabs) => sendCommand(tabs, "run")).catch(onError);
-});
+function addCommandListener(command) {
+  document.getElementById(command).addEventListener("click", (e) => {
+    browser.tabs.query({
+      currentWindow: true,
+      active: true
+    }).then((tabs) => sendCommand(tabs, command)).catch(onError);
+  });
+}
 
-document.getElementById("stop").addEventListener("click", (e) => {
-  browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then((tabs) => sendCommand(tabs, "stop")).catch(onError);
-});
-
-document.getElementById("pause").addEventListener("click", (e) => {
-  browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then((tabs) => sendCommand(tabs, "pause")).catch(onError);
-});
-
-document.getElementById("resume").addEventListener("click", (e) => {
-  browser.tabs.query({
-    currentWindow: true,
-    active: true
-  }).then((tabs) => sendCommand(tabs, "resume")).catch(onError);
-});
+addCommandListener("run");
+addCommandListener("stop");
+addCommandListener("pause");
+addCommandListener("resume");
 
