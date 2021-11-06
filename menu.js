9fb00ae2f5d42999bac0bc6a2b11675537d0d139
zeddan <robin.saaf@gmail.com>
2021-11-06 17:11:01 +0100

    WIP

--- menu.js
+++ menu.js
@@ -1,30 +1,43 @@
 "use strict";
 
+let globalState = "stopped"
+
 function onError(error) {
   alert(error);
   console.error(`Error: ${error}`);
 }
 
-function sendCommand(tabs, command) {
+function sendCommand(tabs, command, state) {
   for (let tab of tabs) {
     browser.tabs.sendMessage(
       tab.id,
-      {command: command}
+      {
+        command: command,
+        state: state
+      }
     ).catch(onError);
   }
 }
 
-function addCommandListener(command) {
+function addCommandListener(command, state) {
   document.getElementById(command).addEventListener("click", (e) => {
     browser.tabs.query({
       currentWindow: true,
       active: true
-    }).then((tabs) => sendCommand(tabs, command)).catch(onError);
+    }).then((tabs) => {
+      globalState = state;
+      setStatus(globalState);
+      sendCommand(tabs, command, state);
+    }).catch(onError);
   });
 }
 
-addCommandListener("run");
-addCommandListener("stop");
-addCommandListener("pause");
-addCommandListener("resume");
+function setStatus(state) {
+  document.getElementById("status").innerText = state;
+}
 
+addCommandListener("run", "running");
+addCommandListener("stop", "stopped");
+addCommandListener("pause", "paused");
+addCommandListener("resume", "running");
+setStatus(globalState)
